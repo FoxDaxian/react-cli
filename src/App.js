cbecf0aeca35a44f4128f9f008e53f99692ce99d
fox <945039036@qq.com>
2017-12-01 10:42:15 +0800

    Update: 迟到的更新

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react'
 // TODO react-routet，修改了提交首页方块不更新的问题
-import {
-	BrowserRouter as Router,
-	Route
-} from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import '@/App.css'
 
 import Header from '@/views/header'
 import Home from '@/views/home'
 
-
-
 class App extends Component {
 	render() {
-		return (<div className="App">
-			<Header></Header>
-			<Router>
-				<Route path="/" component={Home} />
-			</Router>
-		</div>)
+		return (
+			<div className="App">
+				<Header />
+				<Router>
+					<Route path="/" component={Home} />
+				</Router>
+			</div>
+		)
 	}
 }
 
